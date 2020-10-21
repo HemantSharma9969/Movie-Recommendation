e7c1435a99e17ad5b29c1a6121db086e7ef1a9a2
HemantSharma9969 <61488985+HemantSharma9969@users.noreply.github.com>
2020-10-21 22:41:23 +0530

    Update Recommendation.py

--- Recommendation.py
+++ Recommendation.py
@@ -3,45 +3,22 @@ import pandas as pd
 import numpy as np
 import requests
 from bs4 import BeautifulSoup as soup
-import requests
 from tmdbv3api import TMDb
 import json
-import requests
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from PIL import Image
 from tmdbv3api import Movie
 from fuzzywuzzy import fuzz
-import pickle
 
 
 tmdb_movie=Movie()
 tmdb = TMDb()
 tmdb.api_key = '281825c11d7e66ad7a6a8fb94fa92276'
 
-
-
-
-
-
 df = pd.read_csv("main_data.csv")
-cv = CountVectorizer()
-count_matrix = cv.fit_transform(df['comb'])
-similarity = cosine_similarity(count_matrix)
-
-#pickle_in = open("C:\\Users\\Hemant\\Desktop\\Projects\\Recomeend\\AJAX-Movie-Recommendation-System-with-Sentiment-Analysis-master\\similarity.pkl","rb")
-#similarity= pickle.load(pickle_in)
-
-
-movie=df["movie_title"].to_list()
-
-
-
-
-
-
-
+    
 
 
 def result():
@@ -49,8 +26,9 @@ def result():
     image_address = []
     over_view=[]
     name =[]
-
-   
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df['comb'])
+    similarity = cosine_similarity(count_matrix) 
     i = df[df["movie_title"]==m].index[0]
     lst = list(enumerate(similarity[i]))
     lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
@@ -117,46 +95,28 @@ def result():
     
 
 
-
-
 st.title("Movie Recommendation System")
-
-
 m = st.text_input("Enter the Movie Name ","")
-
-
 m = str(m).lower()
 
 
 if m:    
-
-    
     names=[]
     q=dict()
+    movie=df["movie_title"].to_list()
     for i in movie:
         fuzz1=fuzz.token_set_ratio(m, i)
         q.update({i:fuzz1})
 
     s = dict(sorted(q.items(), key=lambda x: x[1],reverse=True)[0:5])
-
     s = list(s.keys())
-
     placeholder = st.empty()
     placeholder.write("Just to make sure there isn't any Spelling Mistake. Please select your movie name from side bar")
-
-
-
-
-
-    
     b1 = st.sidebar.button(s[0].capitalize(), key="1")
     b2 = st.sidebar.button(s[1].capitalize(), key="2")
     b3 = st.sidebar.button(s[2].capitalize(), key="3")
     b4 = st.sidebar.button(s[3].capitalize(), key="4")
     b5 = st.sidebar.button(s[4].capitalize(), key="5")
-
-    
-
     if b1:
         m=str(s[0]).lower()
         result()
