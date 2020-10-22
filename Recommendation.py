e0d32271467056e462dd6d2b940f042d317b0dfe
HemantSharma9969 <61488985+HemantSharma9969@users.noreply.github.com>
2020-10-22 11:19:55 +0530

    Update Recommendation.py

--- Recommendation.py
+++ Recommendation.py
@@ -11,6 +11,11 @@ from nltk.corpus import stopwords
 from PIL import Image
 from tmdbv3api import Movie
 from fuzzywuzzy import fuzz
+import pickle
+
+clf = pickle.load(open('nlp_model.pkl', 'rb'))
+vectorizer = pickle.load(open('tranform.pkl','rb'))
+
 
 
 tmdb_movie=Movie()
@@ -62,6 +67,31 @@ def result():
         
     st.image(images,width =200,caption=names)
     
+    html3 ="""
+    <div style="background-color:black;padding:10px">
+    <h2 style="color:white;text-align:center;">User Reviews </h2>
+    </div>"""
+    st.markdown(html3,unsafe_allow_html=True)
+    
+    imbd = data_json["imdb_id"]
+    url = "https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt".format(imbd)
+    page1 = requests.get(url)
+    page_soup = soup(page1.content,"html.parser",)
+    content = page_soup.find_all("div",{"class":"text show-more__control"})
+
+    reviews_list = [] # list of reviews
+    reviews_status = [] # list of comments (good or bad)
+    for reviews in content:
+
+        reviews_list.append(reviews.text)
+        # passing the review to our model
+        movie_review_list = np.array([reviews.text])
+        movie_vector = vectorizer.transform(movie_review_list)
+        pred = clf.predict(movie_vector)
+        reviews_status.append('Good' if pred else 'Bad')
+
+    df_review = pd.DataFrame(columns= ["Review","Sentiments"],data=list(zip(reviews_list, reviews_status)))
+    st.dataframe(df_review[0:21].style.highlight_max(axis=0))
 
     
     
